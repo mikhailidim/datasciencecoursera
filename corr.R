206da1610ddd18f0719d00f5418c77057411f045
mikhailidim <mikhailidim@gmail.com>
2015-12-12 16:36:20 -0500

    Replace list by data.frame in result. Improves usage in corr.R function

---
+++ corr.R
@@ -0,0 +1,8 @@
+# Coursera: R Programming Assestment 1
+# by Michael Mikhailidi 
+# 12/12/2015 14:35PM
+
+corr <- function(directory,threshold=0) {
+  lst <-subset(complete(directory),nobs>threshold)
+  
+}
